373f529e41332e6cf47f190197001b179f7d1dca
Deniz <202207803@post.au.dk>
2024-03-28 14:20:49 +0100

    tiløfjet SpreadSheet.cs

---
+++ BADASS3/Bakery/Model/SpreadSheet.cs
@@ -0,0 +1,9 @@
+namespace Bakery.Model;
+
+public class SpreadSheet
+{
+    public int SpreadSheetID { get; set; }
+    public int CompanyOrderID { get; set; }
+    public int IngredientStockID { get; set; }
+    public int BakingGoodsListID { get; set; }
+}
