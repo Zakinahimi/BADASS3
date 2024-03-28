134de184b9ff45ed67cdafb037697416c3e3b166
Moneergit <moneerstudy2023@gmail.com>
2024-03-28 14:45:01 +0100

    CompanyOrder

---
+++ BADASS3/Bakery/Model/CompanyOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery.Model
+{
+    public class CompanyOrder
+    {
+        [Key]
+        public int CompanyOrderId { get; set; }
+        [Required] 
+        public int Quantity { get;}
+        [Required]
+        public string BakingGoods { get; set; }
+
+        public ICollection<Supermarket> Supermarket { get; set; }
+
+    }
+}
