ac1686b851f33de36713f5415db48e35688cfe90
Jonas Zaki Nahimi <202109834@post.au.dk>
2024-03-28 14:42:45 +0100

    DispatchSheet model implementet

---
+++ BADASS3/Bakery/Model/DispatchSheet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery.Model
+{
+    public class DispatchSheet
+    {
+        [Key]
+        public int DispatchSheetID { get; set; }
+
+        [ForeignKey("CompanyOrder")]
+        public int CompanyOrderID { get; set; }
+        public virtual CompanyOrder CompanyOrder { get; set; }
+
+        public string TrackID { get; set; }
+        public string Route { get; set; }
+        public string DeliveryPlace { get; set; }
+        public string Signature { get; set; }
+    }
+}
--- BADASS3/Bakery/Model/fewfwefwe.cs
+++
@@ -1,7 +0,0 @@
-class MainClass
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
