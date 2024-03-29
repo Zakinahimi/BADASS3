38a88e37efd2d4f74034ce22dcaa1bbbd30d4714
Jonas Zaki Nahimi <202109834@post.au.dk>
2024-03-29 17:42:35 +0100

    Created IngredientsStock model

---
+++ BADASS3/Bakery/Model/IngredientsStock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery.Model
+{
+    public class IngredientsStock
+    {
+        [Key]
+        public int IngredientsStockID { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string Ingredient { get; set; }
+
+        public ICollection<SpreadSheet> SpreadSheet { get; set; }
+    }
+}
