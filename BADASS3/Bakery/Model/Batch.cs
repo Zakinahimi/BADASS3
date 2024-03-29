9ce8bc10aa83e5a6ca7266c58f9b5ded8ff6261f
Jonas Zaki Nahimi <202109834@post.au.dk>
2024-03-29 17:51:31 +0100

    added batch model

---
+++ BADASS3/Bakery/Model/Batch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Bakery.DTO;
+
+namespace Bakery.Model
+{
+    public class Batch
+    {
+        [Key]
+        public int BatchID { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime TargetFinishTime { get; set; }
+
+        [Required]
+        public DateTime ActualFinishTime { get; set; }
+
+
+        [ForeignKey("BakingGoodsList")]
+        public int BakingGoodsListID { get; set; }
+        public virtual BakingGoodsList BakingGoodsList { get; set; }
+
+        [ForeignKey("IngredientsStock")]
+        public int IngredientsStockID { get; set; }
+        public virtual IngredientsStock IngredientsStock { get; set; }
+
+
+
+        public ICollection<Schedule> Schedules { get; set; }
+    }
+}
