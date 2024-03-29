f88b1fa22a8e5fde253add08ec5c1ade94e5fdb6
Jonas Zaki Nahimi <202109834@post.au.dk>
2024-03-29 18:19:30 +0100

    add Schedule model

--- BADASS3/Bakery/Model/Schedule.cs
+++ BADASS3/Bakery/Model/Schedule.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Bakery.DTO;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bakery.Model
 {
-    internal class Schedule
+    public class Schedule
     {
+        [Key]
+        public int ScheduleID { get; set; }
+
+        [ForeignKey("BakingGoodsList")]
+        public int BakingGoodsListID { get; set; }
+        public virtual BakingGoodsList BakingGoodsList { get; set; }
+
+        [ForeignKey("Batch")]
+        public int BatchID { get; set; }
+        public virtual Batch Batch { get; set; }
+
+        [ForeignKey("IngredientsStock")]
+        public int IngredientsStockID { get; set; }
+        public virtual IngredientsStock IngredientsStock { get; set; }
     }
 }
