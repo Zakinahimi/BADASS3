2dc9bfcb22d5efd4597db241c3f025ccbd88717a
Moneergit <moneerstudy2023@gmail.com>
2024-04-02 13:31:58 +0200

    seedcon

---
+++ BADASS3/Bakery/Controllers/SeedController.cs
@@ -0,0 +1,16 @@
+﻿using Bakery.Database;
+using Bakery.Model;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Bakery.Controllers
+{
+    public class SeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
