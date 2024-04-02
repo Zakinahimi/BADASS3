9eea7dda2efeec04eca418e0c25bc599965e3dd3
Moneergit <moneerstudy2023@gmail.com>
2024-04-02 13:54:02 +0200

    Update SeedController.cs

--- BADASS3/Bakery/Controllers/SeedController.cs
+++ BADASS3/Bakery/Controllers/SeedController.cs
@@ -2,13 +2,17 @@
 using Bakery.Model;
 using Microsoft.AspNetCore.Mvc;
 
-
-
 namespace Bakery.Controllers
 {
     public class SeedController : Controller
     {
-        private BakeryDbContext _context;
-        public SeedController(BakeryDbContext context) => _context = context;
+        private readonly BakeryDbContext _context;
+        public SeedController(BakeryDbContext context)
+        {
+            _context = context;
+        }
+
+
     }
+
 }
