fb72fded5b849d32c1fbcc076e28b4df41f59ae4
Deniz <202207803@post.au.dk>
2024-04-05 14:32:42 +0200

    tilføjet kode til migration_allergen

--- BADASS3/Bakery/Migrations/20240404173322_migration1_allergen.cs
+++ BADASS3/Bakery/Migrations/20240404173322_migration1_allergen.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Bakery.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,13 +12,21 @@ namespace Bakery.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<string>(
+                name: "Allergen",
+                table: "Ingredient",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
 
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "Allergen",
+                table: "Ingredient");
         }
     }
 }
